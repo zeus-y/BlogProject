3eb06d7eaeada712b7dd3da5b2a85568c6a06f20
zeus-y <48348599+zeus-y@users.noreply.github.com>
2020-08-27 20:15:46 +0800

    Update settings.py

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -23,7 +23,6 @@ sys.path.insert(0, os.path.join(BASE_DIR, 'extra_apps'))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '@7$yyhgy@sz7xf^^o!t^3*@03*^r6*l8!)xji9z0%v*ppkds)q'
 SECRET_KEY = 'xm0k4%(m+7-vgopvkjk0#e8-$b%f03k)2nhn@+e5km1)0cz4-5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
